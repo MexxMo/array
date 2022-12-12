260dbb50c8b29a51d9c501fc93a1bb2a7cd73ba8
Ildar <mexx0174@gmail.com>
2022-12-12 14:13:54 +0500

    Массив 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+
+        int [] array1 = new int[]{1,2,3};
+        double[] array2 = new double[]{1.57, 7.654, 9.986};
+        int [] array3 = {123,123,213123,123};
+
+        System.out.println("Задание 2");
+
+        System.out.print(Arrays.toString(array1));
+
+
+
+
+    }
+}
